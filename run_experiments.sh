f0edd0a496698ddc2dc65b72534a801ded9d9fc1
jayesh59 <jayeshnagpal59@gmail.com>
2025-03-14 01:44:36 -0700

    added paths

--- run_experiments.sh
+++ run_experiments.sh
@@ -14,11 +14,13 @@ if [[ -z "$name" ]];then
     name=1
 fi
 
+SOURCE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 log_folder=$domain"_logs"
 path="$(pwd)"
 domain_model_folder=$domain"_model_files"
 
-bash $path/LAMP/experiments_setup.sh -d $domain
+bash $path/OLAMP/experiments_setup.sh -d $domain
 wait
 
 array=()
@@ -56,14 +58,14 @@ do
         seed2=0
         seed_model_folder="seed_"$seed"_order_"$seed2
 
-        bash -c "cp -r /workspaces/$domain/misc/$t""_$seed""_*.p /workspaces/LAMP/Data/$domain/misc/ && echo $t files added"
+        bash -c "cp -r $SOURCE/$domain/misc/$t""_$seed""_*.p $SOURCE/OLAMP/Data/$domain/misc/ && echo $t files added"
         wait
 
         echo "different order seed = $seed2"
-        bash -c "python $path/LAMP/timed_experiment.py -n $name -r $robot --domain $domain --problem_num $problem_num --seed $seed --total_demo_count $t --seed2 $seed2"
+        bash -c "python $path/OLAMP/timed_experiment.py -n $name -r $robot --domain $domain --problem_num $problem_num --seed $seed --total_demo_count $t --seed2 $seed2"
         wait
 
-        bash -c "mkdir -p /workspaces/$domain_model_folder/$seed_model_folder && mv /workspaces/LAMP/Data/$domain/misc/$t""_$seed""_* /workspaces/$domain_model_folder/$seed_model_folder/ && echo $t files moved"
+        bash -c "mkdir -p $SOURCE/$domain_model_folder/$seed_model_folder && mv $SOURCE/OLAMP/Data/$domain/misc/$t""_$seed""_* $SOURCE/$domain_model_folder/$seed_model_folder/ && echo $t files moved"
         wait
     done
 done
